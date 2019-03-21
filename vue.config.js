ff178e2033c6678dc36bd3de30dd87804710a484
opacitySun <809340810@qq.com>
2019-03-21 10:14:17 +0800

    v1.0

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
     devServer: {
         host: '0.0.0.0',
         hot: true,
-        disableHostCheck: true,
+        disableHostCheck: true,	//解决未配置的hostname访问时报invalid host header的问题
     },
 }
