61e391a2814a7abd61200fd773086c7358994dc9
opacitySun <809340810@qq.com>
2019-04-11 16:15:24 +0800

    v1.0

--- vue.config.js
+++ vue.config.js
@@ -14,6 +14,7 @@ module.exports = {
                 target: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',
                 changeOrigin: true
             }
-        }
+        },
+        disableHostCheck: true
     }
 }
